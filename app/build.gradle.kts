d691a041be01b53e75b7963d3929ff2d632b1d6f
taron <jgye8530@gmail.com>
2023-11-21 12:13:57 +0900

    loginModify

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,9 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.retrofit2:converter-scalars:2.9.0")
-  
+
+    implementation ("com.kakao.sdk:v2-user:2.18.0")
+
 
 
 }
