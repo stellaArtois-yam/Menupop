232a06094913e175be352cf43385ef431ebc0cd2
taron <jgye8530@gmail.com>
2023-11-27 19:22:01 +0900

    exchangeRateFragment add

---
+++ app/src/main/java/com/example/menupop/mainActivity/ExchangeModel.kt
@@ -0,0 +1,56 @@
+package com.example.menupop.mainActivity
+
+import android.content.SharedPreferences
+import android.util.Log
+import com.example.menupop.RetrofitService
+import com.example.menupop.login.LoginResponseModel
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+class ExchangeModel {
+    val TAG = "ExchangeModel"
+
+    val gson: Gson = GsonBuilder()
+        .setLenient()
+        .create()
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://www.koreaexim.go.kr/site/program/financial/") // 실제 서버의 기본 URL로 대체해야 합니다.
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .build()
+
+    private val service = retrofit.create(RetrofitService::class.java)
+    fun requestExchangeRate(authkey: String,callback: (ArrayList<ExchangeDataClass.ExchangeDataClassItem>) -> Unit){
+        service.requestExchangeRate(authkey,"20231123","AP01")
+//        service.requestExchangeRate(authkey,"AP01")
+        .enqueue(object : Callback<ArrayList<ExchangeDataClass.ExchangeDataClassItem>>{
+            override fun onResponse(
+                call: Call<ArrayList<ExchangeDataClass.ExchangeDataClassItem>>,
+                response: Response<ArrayList<ExchangeDataClass.ExchangeDataClassItem>>
+            ) {
+                if(response.isSuccessful){
+                    callback(response.body()!!)
+                }
+            }
+
+            override fun onFailure(
+                call: Call<ArrayList<ExchangeDataClass.ExchangeDataClassItem>>,
+                t: Throwable
+            ) {
+                Log.d(TAG, "onFailure: ${t}")
+            }
+
+        })
+    }
+    fun exchangeRateApplicationStatus(sharedPreferences: SharedPreferences, status : Boolean){
+        var editor = sharedPreferences.edit()
+        editor.putBoolean("exchangeRateApplicationStatus",status)
+        editor.apply()
+    }
+}
